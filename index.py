5d9d937a553c79a2a87dbfcfef1cd82f5fd326b7
Christian Monaghan <christian.monaghan@pm.me>
2024-09-02 11:46:55 -0400

    add route to serve favicon

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, send_from_directory
 from ai import get_fallacies
 
 app = Flask(__name__)
@@ -6,6 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/api', methods=['POST'])
 def api():
